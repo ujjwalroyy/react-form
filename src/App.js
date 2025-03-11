64f244674691d332c827619b131721a8b7985ed1
Yugal Mittal <yugal@braintechnosys.com>
2025-03-11 13:15:31 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Display from './component/Display';
 import Form from './component/Form';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Form/>
-      <Display/>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/' element={<Home/>}/> */}
+          <Route path="/display" element={<Display/>} />
+          <Route path="/form" element={<Form/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
